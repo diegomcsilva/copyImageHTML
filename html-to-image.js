a768d4c43ef984ae72c1b4d450447939959f1b5d
Diego Marcelo <diego.marcelo@tvglobo.com.br>
2019-12-19 16:46:55 -0300

    Generate image with node

---
+++ html-to-image.js
@@ -0,0 +1,7 @@
+const nodeHtmlToImage = require('node-html-to-image')
+
+nodeHtmlToImage({
+  output: './image.png',
+  html: '<html><body>Hello world!</body></html>'
+})
+  .then(() => console.log('The image was created successfully!'))
